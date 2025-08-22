93eef3f4c00be2762882d1b696b07a274d76ac7b
Faizan Ul Ghani <faizanulghani1@gmail.com>
2025-08-22 16:26:12 +0500

    created Basic Backend

--- Backend/app.js
+++ Backend/app.js
@@ -1,16 +1,37 @@
 import express from "express";
 import { nanoid } from "nanoid";
+import dotenv from "dotenv";
+import connectDB from "./src/config/mongo.config.js";
+import urlSchema from "./src/models/shorturl.model.js";
+
+dotenv.config();
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/api/create", (req, res) => {
+app.post("/api/create", (req, res) => {
   const { url } = req.body;
+  const shortUrl = nanoid(7);
+  const newUrl = new urlSchema({
+    full_url: url,
+    short_url: shortUrl,
+  });
+  newUrl.save();
+  res.send(nanoid(7));
+});
 
-  res.send("Hello World!");
+app.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  const url = await urlSchema.findOne({ short_url: id });
+  if (url) {
+    res.redirect(url.full_url);
+  } else {
+    res.status(404).send("URL not found");
+  }
 });
 
 app.listen(3000, () => {
-  console.log("Server is running on port 5000");
+  console.log("Server is running on port 3000");
+  connectDB();
 });
