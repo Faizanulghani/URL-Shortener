e3e3d095b268029690a394e3845567917f4bf58f
Faizan Ul Ghani <faizanulghani1@gmail.com>
2025-08-26 07:31:18 +0500

    Added tryCatchWrapper

--- Backend/src/controller/shortUrl.controller.js
+++ Backend/src/controller/shortUrl.controller.js
@@ -1,14 +1,16 @@
 import { getShortUrl } from "../dao/shortUrl.js";
 import { createShortUrlWithoutServices } from "../services/shortUrl.service.js";
+import wrapAsync from "../utils/tryCatchWrapper.js";
 
-export const createShortUrl = async (req, res) => {
+export const createShortUrl = wrapAsync(async (req, res) => {
   const { url } = req.body;
   const shortUrl = await createShortUrlWithoutServices(url);
   res.send(process.env.APP_URL + shortUrl);
-};
+});
 
-export const redirectFromShortUrl = async (req, res) => {
+export const redirectFromShortUrl = wrapAsync(async (req, res) => {
   const shortUrl = req.params.id;
   const url = await getShortUrl(shortUrl);
+  if (!url) throw new Error("Short URL not found");
   res.redirect(url);
-};
+});
