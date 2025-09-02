b79daa08084ead21edaebe9b04c1fdab6be70912
Faizan Ul Ghani <faizanulghani1@gmail.com>
2025-09-02 16:10:00 +0500

    Fixed Routing Issues

--- Frontend/src/store/store.js
+++ Frontend/src/store/store.js
@@ -1,10 +1,10 @@
-import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "./slice/authSlice.js";
+// import { configureStore } from "@reduxjs/toolkit";
+// import authReducer from "./slice/authSlice.js";
 
-export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-  },
-});
+// export const store = configureStore({
+//   reducer: {
+//     auth: authReducer,
+//   },
+// });
 
-export default store;
+// export default store;
