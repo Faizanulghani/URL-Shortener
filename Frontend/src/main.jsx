ff90e460a150a544e974d6c9736d92fc6484a4be
Faizan Ul Ghani <faizanulghani1@gmail.com>
2025-09-02 16:02:29 +0500

    Added Routing

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,12 +1,21 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { routeTree } from "./routing/routeTree.js";
+import store from "./store/store.js";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
+export const queryClient = new QueryClient();
+const router = createRouter({
+  routeTree,
+  context: {
+    queryClient,
+    store,
+  },
+});
 
 createRoot(document.getElementById("root")).render(
   <QueryClientProvider client={queryClient}>
-    <App />
+    <RouterProvider router={router} />
   </QueryClientProvider>
 );
