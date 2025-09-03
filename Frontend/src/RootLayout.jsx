2f54877819140204c9d46ad1825e6669cc71ad4f
Faizan Ul Ghani <faizanulghani1@gmail.com>
2025-09-03 14:46:24 +0500

    Fixed some issues

--- Frontend/src/RootLayout.jsx
+++ Frontend/src/RootLayout.jsx
@@ -2,20 +2,17 @@ import { Outlet, useNavigate } from "@tanstack/react-router";
 import Navbar from "./components/NavBar";
 import { useDispatch, useSelector } from "react-redux";
 import { logout } from "./store/slice/authSlice";
+import { logoutUser } from "./api/user.api";
 
 const RootLayout = () => {
   const auth = useSelector((state) => state.auth);
-  
+
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const handleLogout = async () => {
     try {
-      await fetch("http://localhost:3000/api/auth/logout", {
-        method: "POST",
-        credentials: "include",
-      });
-
+      await logoutUser();
       dispatch(logout());
       navigate({ to: "/auth" });
     } catch (err) {
